8c50da72520e1715b03ca39f01ded42dd93f5f53
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2024-03-17 10:55:58 +0100

    refactor(ui): enhance SquareColor component for improved flexibility

--- src/ui/SquareColor.jsx
+++ src/ui/SquareColor.jsx
@@ -4,10 +4,10 @@ const SquareColor = styled.div.attrs((props) => ({
   style: {
     backgroundColor: props.$main,
     borderColor: props.$secondary,
+    width: props.width || "4rem",
+    height: props.height || "4rem",
   },
 }))`
-  width: 4rem;
-  height: 4rem;
   border-width: 1rem;
   border-style: solid;
   outline: 1px solid var(--color-grey-400);
