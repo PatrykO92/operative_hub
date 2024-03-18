ba1a684e18bdcc65b6879eda99b000d9c93905f5
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2024-03-18 23:30:35 +0100

    feat: Remove refetch

--- src/hooks/useGetOrdersList.js
+++ src/hooks/useGetOrdersList.js
@@ -9,7 +9,6 @@ export function useGetOrdersList() {
   } = useQuery({
     queryKey: ["orders"],
     queryFn: getOrdersList,
-    refetchInterval: 5000,
   });
 
   return { isLoadingOrders, orders, orderListError };
